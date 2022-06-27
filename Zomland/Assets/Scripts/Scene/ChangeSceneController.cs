1ec62b0e290815b97b29a58336d2907fd9d6b19a
Lê Nguyễn Quang Minh <lnqminh3003@gmail.com>
2022-06-27 19:35:18 +0700

    Change Color

--- Zomland/Assets/Scripts/Scene/ChangeSceneController.cs
+++ Zomland/Assets/Scripts/Scene/ChangeSceneController.cs
@@ -10,32 +10,3 @@ public static class ChangeSceneController
         SceneManager.LoadScene(sceneTo);
     }
 }
-
-/// <summary>
-    /// This attribute is used to represent a string value
-    /// for a value in an enum.
-    /// </summary>
-    public class StringValueAttribute : System.Attribute {
-
-        #region Properties
-
-        /// <summary>
-        /// Holds the stringvalue for a value in an enum.
-        /// </summary>
-        public string StringValue { get; protected set; }
-
-        #endregion
-
-        #region Constructor
-
-        /// <summary>
-        /// Constructor used to init a StringValue Attribute
-        /// </summary>
-        /// <param name="value"></param>
-        public StringValueAttribute(string value) {
-            this.StringValue = value;
-        }
-
-        #endregion
-
-    }
